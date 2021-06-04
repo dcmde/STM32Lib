4d1591daddabefd6c6d35f25dda0bad97e1737f1
dcmde <43622430+dcmde@users.noreply.github.com>
2021-06-04 20:28:25 +0200

    Use of the new spi interface.

--- drivers/src/ff11/diskio-spi-wrapper.c
+++ drivers/src/ff11/diskio-spi-wrapper.c
@@ -1,24 +1,26 @@
 #include <spi.h>
+#include <stdio.h>
 #include "diskio-spi-wrapper.h"
 
+extern SPI_t *spi_miroSd;
+
 void SELECT(void) {
-    spi_slave_enable();
+    printf("select\n");
+    spi_slave_enable(spi_miroSd);
 }
 
 void DESELECT(void) {
-    spi_slave_disable();
+    spi_slave_disable(spi_miroSd);
 }
 
 void xmit_spi(BYTE Data) {
-    spi_send_receive(Data);
+    spi_send_receive(spi_miroSd, Data);
 }
 
 BYTE rcvr_spi(void) {
-    unsigned char Dummy, Data;
+    unsigned char Dummy;
     Dummy = 0xFF;
-    Data = 0;
-    Data = spi_send_receive(Dummy);
-    return Data;
+    return spi_send_receive(spi_miroSd, Dummy);
 }
 
 void rcvr_spi_m(BYTE *dst) {
