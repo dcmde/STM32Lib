71354b7787214356763aead53449d5145c48683e
dcmde <43622430+dcmde@users.noreply.github.com>
2021-06-05 16:06:00 +0200

    Commented unused functions for printf / debugging

--- stm32f1_hal/src/stm32f103_dbg.c
+++ stm32f1_hal/src/stm32f103_dbg.c
@@ -1,9 +1,7 @@
 #include "stm32f103_dbg.h"
-
 #include <stm32f10x_rcc.h>
-#include <stm32f10x_tim.h>
 
-volatile uint32_t DBG_TimeStamp = 0;
+//volatile uint32_t DBG_TimeStamp = 0;
 
 /**
 	* @about Redefine write function to send data through SWO.
@@ -36,35 +34,34 @@ void DBG_Init(void){
 	AFIO->MAPR = tmp;
 }
 
-
-void DBG_InitTimestamp(DBG_TimestampFreq dbg_timestampFreq){
-
-	TIM_TimeBaseInitTypeDef TIM_InitStruct;
-
-	DBG_Init();
-
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
-
-	TIM_InitStruct.TIM_ClockDivision = TIM_CKD_DIV1;
-	TIM_InitStruct.TIM_CounterMode = TIM_CounterMode_Up;
-	TIM_InitStruct.TIM_Period = (uint16_t)18*dbg_timestampFreq;
-	TIM_InitStruct.TIM_Prescaler = 3;
-	TIM_InitStruct.TIM_RepetitionCounter = 0;
-
-	TIM_TimeBaseInit(TIM3, &TIM_InitStruct);
-
-	TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);
-	NVIC_EnableIRQ(TIM3_IRQn);
-
-	TIM_Cmd(TIM3, ENABLE);
-
-}
-
-void DBG_PrintTimestamp(void){
-    printf("%lu\n", DBG_TimeStamp);
-}
-
-void TIM3_IRQHandler(void){
-    ++DBG_TimeStamp;
-    TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
-}
+//void DBG_InitTimestamp(DBG_TimestampFreq dbg_timestampFreq){
+//
+//	TIM_TimeBaseInitTypeDef TIM_InitStruct;
+//
+//	DBG_Init();
+//
+//	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+//
+//	TIM_InitStruct.TIM_ClockDivision = TIM_CKD_DIV1;
+//	TIM_InitStruct.TIM_CounterMode = TIM_CounterMode_Up;
+//	TIM_InitStruct.TIM_Period = (uint16_t)18*dbg_timestampFreq;
+//	TIM_InitStruct.TIM_Prescaler = 3;
+//	TIM_InitStruct.TIM_RepetitionCounter = 0;
+//
+//	TIM_TimeBaseInit(TIM3, &TIM_InitStruct);
+//
+//	TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);
+//	NVIC_EnableIRQ(TIM3_IRQn);
+//
+//	TIM_Cmd(TIM3, ENABLE);
+//
+//}
+//
+//void DBG_PrintTimestamp(void){
+//    printf("%lu\n", DBG_TimeStamp);
+//}
+//
+//void TIM3_IRQHandler(void){
+//    ++DBG_TimeStamp;
+//    TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
+//}
